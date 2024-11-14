43fbec0c189cff4c1fd0e5fbfcecede445218ed6
Tryon <tryon@visibleone.com>
2024-11-14 20:00:35 +0700

    setup rsd 2025

---
+++ yaycha/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import ThemedApp from './ThemedApp.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemedApp />
+  </StrictMode>,
+)
